cf9894ad8181249aa7ea0741798d3044beee7ade
LeppoXena <francesco.lepore@xenagames.com>
2022-05-18 19:05:12 +0200

    Function applys correctly

--- js/myscript.js
+++ js/myscript.js
@@ -57,17 +57,21 @@ let team = [];
 team.push(empl00,empl01,empl02,empl03,empl04,empl05);
 console.log(team);
 
-let logSample = team.indexOf("empl04");
+  console.log(Object.keys(empl05));
+
+
+
+let logSample = empl03.src;
 
 console.log(logSample);
 
 // aggiungo il node dove appendere la mia array
 
-const deploy = document.querySelector("div.team-container");
+var deploy = document.querySelector("div.team-container");
 
 // Lancio una funzione per appendere i vari dati alla pagina
 
-// addEmpl (team,deploy);
+addEmpl (team,deploy);
 
 // inizio a fare la prima
 
@@ -76,21 +80,36 @@ function addEmpl (arrayEmp, node) {
 
         for (let i = 0; i < arrayEmp.length; i++) {
             const empl = arrayEmp[i];
+
             let card = document.createElement("div");
             card.className = "team-card";
             node.append(card);
 
-            for (let chiave in empl) {
-                console.log (chiave + ": " + empl [chiave])
-                
-            }
+            let cardImg = document.createElement("div");
+            cardImg.className = "card-image";
+            card.append(cardImg);
 
-            // let pic = document.createElement("img");
-            // pic.src = arrayEmp (i,3);
-            // card.append(pic);
+            let emplName = empl.name;
+            let emplRole = empl.role;
+            let emplPic = empl.src;
+            
+            let pic = document.createElement("img");
+            pic.src = emplPic;
+            pic.alt = emplName
+            cardImg.append(pic);
 
+            let cardTxt = document.createElement("div");
+            cardTxt.className = "card-text";
+            card.append(cardTxt);
+
+            let txtName = document.createElement("h3");
+            txtName.append(emplName);
+            cardTxt.append(txtName);
+
+            let txtRole = document.createElement("p");
+            txtRole.append(emplRole);
+            cardTxt.append(txtRole);
         }
-
 }
 
 {/* <div class="team-card">
